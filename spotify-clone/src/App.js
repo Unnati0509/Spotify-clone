b08bf10303081e47e13bcec761495109183956cb
Unnati <unnatisaxena1605@gmail.com>
2023-06-26 18:45:40 +0530

    Made datalayer

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './Login'
-
+import { getTokenFromURL } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './Datalayer';
+// import { useEffect } from 'react';
+const spotify = new SpotifyWebApi();
 function App() {
+  const[token,setToken] = useState(null);
+  const [{user},dispatch]=useDataLayerValue();
+  //run code based on a given condition
+  useEffect(()=>{
+    const hash = getTokenFromURL();
+    window.location.hash="";
+    const _token = hash.access_token;
+    // console.log('I have a token ->',token)
+    if(_token){
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+     
+        dispatch({
+          type:'SET_USER',
+          user:user,
+        })
+      })
+    }
+
+  },[]);
+  console.log('🤠',user);
   return (
-    <div>
-      <Login/>
+    <div className='app'>
+    {
+      token ?<Player /> 
+
+      :  <Login/>
+    }
+     
     </div>
   )
 }
--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -11,5 +11,14 @@ const scopes = [
     "user-top-read",
     "user-modify-playback-state",
 ];
-
+export const getTokenFromURL = () =>{
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial,item)=>{
+        let parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial;
+    },{});
+}
 export const loginURL = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectURI}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
